503454656dd6408a2ffb0478b139e6a432d057c6
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-07-21 19:47:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// writecards function
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages; 
+  }
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const thankYouCards = writeCards(names, eventName);
+  console.log(thankYouCards);
+  
+// countdown function
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  countDown(10);
